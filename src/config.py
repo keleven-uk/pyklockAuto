e33dbe9466caf6a2ebe220f13d97dfa25cda9795
Kevin Scott <github@keleven.co.uk>
2026-05-19 21:07:40 +0100

    Created a new branch as a backup of the main branch - FileSystemModel. 	This branch uses the QT FileSystemModel to scan a directory and display the files ina list view. 	The works, but I'm not sure how I can get at the individual files with manually scanning the directory myself. 	If I go down this path, I might as well use this method to also populate the list box. 	I have used this method in other projects and will use to fill the file store - to be used shortly.

--- src/config.py
+++ src/config.py
@@ -171,7 +171,7 @@ class Config():
         written = strNow.strftime("%A %d %B %Y  %H:%M:%S")
         config  = dict()
 
-        config["INFO"] = {"myVERSION": "2026.1",
+        config["INFO"] = {"myVERSION": "2026.3",
                           "myNAME"   : "pyklockAuto"}
 
         config["APPLICATION"] = {"x_pos"      : 100,
